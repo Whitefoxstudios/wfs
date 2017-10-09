67fe587d11c0750e0782a5b1996c62ee5648d66d
Whitefoxstudios <support@whitefoxstudios.net>
2017-10-09 10:10:27 -0400

    Cleaned

--- js/whitefox.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  $(document).ready(function(){
-    $.getScript("/sites/all/modules/wfs/js/modernizr.js");
-    $.getScript("/sites/all/modules/wfs/js/migrate.js");
-    $.getScript("/sites/all/modules/wfs/js/fix.js");
-  });
-})(jQuery);
